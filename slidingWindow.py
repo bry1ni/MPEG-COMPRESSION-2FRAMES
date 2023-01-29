281acd0d527f1f4de108b199c6190a984f06e6f4
Ryan <106076362+bry1ni@users.noreply.github.com>
2023-01-29 21:46:49 +0100

    Update slidingWindow.py

--- slidingWindow.py
+++ slidingWindow.py
@@ -46,8 +46,8 @@ def slidingWindow(padding):
             if MIN > 50:
                 greens.append((y, x))
                 reds.append((j, i))
-                print(f'found and inserted. MIN: {MIN}')
-                newImg[x:x + boxSize, y:y + boxSize] = blocVert  # adding to the matrix
+                blocRes = blocRouge - blocVert
+                newImg[i:i + boxSize, j:j + boxSize] = blocRes  # adding to the matrix
     return newImg, greens, reds
 
 
@@ -67,12 +67,12 @@ start_time = time.time()
 print('starting...')
 padding = 7  # ps: padding bigger = longer time
 ourImage, greensR, redsR = slidingWindow(padding)
-drawRectanglesForSlidingWindow(img92, greensR, img72, redsR)
+#drawRectanglesForSlidingWindow(img92, greensR, img72, redsR)
 print('finish.')
 time = time.time() - start_time
 print(f"{time} seconds")
 imgResu = Image.fromarray(ourImage)  # converting the matrix into image
 imgResu.save('images/SlidingWindowResults/imageResuSlidingWindow.png')
-cv2.imwrite('images/SlidingWindowResults/image072withRedRect.png', img72)  # u can use just cv2.show()
-cv2.imwrite('images/SlidingWindowResults/image092withGreenRect.png', img92)  # u can use just cv2.show()
+#cv2.imwrite('images/SlidingWindowResults/image072withRedRect.png', img72)  # u can use just cv2.show()
+#cv2.imwrite('images/SlidingWindowResults/image092withGreenRect.png', img92)  # u can use just cv2.show()
 sys.exit()
