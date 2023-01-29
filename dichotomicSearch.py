c1e002ed69859a9113a120c00ee3f0e82e805f7b
Ryan <106076362+bry1ni@users.noreply.github.com>
2023-01-29 21:14:07 +0100

    Update dichotomicSearch.py

--- dichotomicSearch.py
+++ dichotomicSearch.py
@@ -1,12 +1,14 @@
 import random
 import sys
+from itertools import product
+
 from PIL import Image
 import cv2
 import numpy as np
 from math import inf
 import time
 
-# --- GLOBAL DECLARATIONS --- 
+# --- GLOBAL DECLARATIONS ---
 
 img72 = cv2.imread('images/OriginalImages/image072.png')
 img72padding = cv2.imread('images/OriginalImages/image072padding.png')
@@ -15,43 +17,46 @@ grayImg72 = cv2.cvtColor(img72, cv2.COLOR_RGB2YCrCb)[:, :, 0]
 grayImg92 = cv2.cvtColor(img92, cv2.COLOR_RGB2YCrCb)[:, :, 0]
 newGrayImg72 = cv2.cvtColor(img72padding, cv2.COLOR_RGB2YCrCb)[:, :, 0]
 
+green = (0, 255, 0)
+red = (0, 0, 255)
+
 BOXSIZE = 16
-height, width, channels = img72.shape
+height, width, channels = img92.shape
 
 # --- CREATING THE NEW IMAGE EMPTY ---
 newImg = np.zeros((height, width), dtype=np.uint8)
 
 
 # --- FUNCTIONS ---
-
 def MSE(bloc1, bloc2):
     block1, block2 = np.array(bloc1), np.array(bloc2)
     return np.square(np.subtract(block1, block2)).mean()
 
 
-def dichotomicSearch(bloc, pointi, pointj, minmse):
+def dichotomicSearch(bloc, pointi, pointj):
     global y, x, bloc72
     stepToVosin = 32
-    debut_i = pointi - stepToVosin - 8 + 64
-    debut_j = pointj - stepToVosin - 8 + 64
+    debut_i = pointi + 64
+    debut_j = pointj + 64
+    minmse = inf
     """ (debut_i, debut_j) is the top left point of the 9 blocks """
     while stepToVosin >= 1:
         fin = stepToVosin * 2 + BOXSIZE
-        for newi in range(int(debut_i), int(debut_i) + int(fin), int(stepToVosin)):
-            for newj in range(int(debut_j), int(debut_j) + int(fin), int(stepToVosin)):
-                # Cropping the bloc from the 072image
-                bloc72 = newGrayImg72[newi:newi + BOXSIZE, newj:newj + BOXSIZE]
-                # Calculating the MSE between the two blocks
-                loss = MSE(bloc, bloc72)
-                if loss < minmse:
-                    minmse = loss
-                    x = newi
-                    y = newj
-        stepToVosin /= 2
+        for p in product([debut_i - stepToVosin, debut_i, debut_i + stepToVosin],
+                         [debut_j - stepToVosin, debut_j, debut_j + stepToVosin]):
+            # Cropping the bloc from the 072image
+            px, py = int(p[0] - 8), int(p[1] - 8)
+            bloc72 = newGrayImg72[px:px + BOXSIZE, py:py + BOXSIZE]
+            # Calculating the MSE between the two blocks
+            loss = MSE(bloc, bloc72)
+            if loss < minmse:
+                minmse = loss
+                x, y = p
+        stepToVosin = int(stepToVosin / 2)
         # We continue from the block with the smallest MSE between the 9 blocks after every step
         # So the affectation of (x,y) into (debut_i, debut_j) is required
-        debut_i = x - stepToVosin - 8
-        debut_j = y - stepToVosin - 8
+        debut_i = x
+        debut_j = y
 
     # We return the coordinates of the last block ( when step == 1 )
     return x, y, minmse, bloc72
@@ -62,9 +67,8 @@ def main():
         for j in range(0, width - BOXSIZE, BOXSIZE):  # WIDTH
             # Cropping the bloc from the 092image
             bloc92 = grayImg92[i:i + BOXSIZE, j:j + BOXSIZE]
-            MINmse = inf
             # Applying the function
-            x, y, minmse, bloc = dichotomicSearch(bloc92, i + 8, j + 8, MINmse)  # +8 for the center of the bloc
+            x, y, minmse, bloc = dichotomicSearch(bloc92, i + 8, j + 8)  # +8 for the center of the bloc
             x -= 72
             y -= 72
             # Checking the MSE value
@@ -74,13 +78,10 @@ def main():
                 # We add our resédu-bloc to our EMPTY IMAGE
                 newImg[i:i + BOXSIZE, j:j + BOXSIZE] = blocRes
                 # Generating colors and draw rectangles on the two images
-                r = random.randint(0, 255)
-                g = random.randint(0, 255)
-                b = random.randint(0, 255)
                 cv2.rectangle(img92, (j, i),
-                              (j + BOXSIZE, i + BOXSIZE), (r, g, b), 2)
+                              (j + BOXSIZE, i + BOXSIZE), red, 2)
                 cv2.rectangle(img72, (y, x),
-                              (y + BOXSIZE, x + BOXSIZE), (r, g, b), 2)
+                              (y + BOXSIZE, x + BOXSIZE), green, 2)
 
 
 # --- MAIN PROGRAM ---
@@ -96,6 +97,6 @@ if __name__ == '__main__':
     imgResu = Image.fromarray(newImg)
     # Saving the results
     imgResu.save('images/dichotomicSearchResults/imageResuDichoto.png')
-    cv2.imwrite('images/dichotomicSearchResults/image072withRedRect.png', img72)  # u can use just cv2.show()
-    cv2.imwrite('images/dichotomicSearchResults/image092withGreenRect.png', img92)
+    cv2.imwrite('images/dichotomicSearchResults/image072withGreenRect.png', img72)  # u can use just cv2.show()
+    cv2.imwrite('images/dichotomicSearchResults/image092withRedRect.png', img92)
     sys.exit()
